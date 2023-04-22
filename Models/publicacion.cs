52eab4c3574b90523f53608e33ebc2fdaa552fee
leonardopadillla <84750145+leonardopadillla@users.noreply.github.com>
2023-04-22 09:49:07 -0600

    Update

--- Models/publicacion.cs
+++ Models/publicacion.cs
@@ -6,7 +6,7 @@ namespace LP022018UP6012019MA603.Models
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public int UsuarioId { get; set; }
-        public Usuario Usuario { get; set; } 
+        public usuario Usuario { get; set; } 
         public List<comentario> Comentario { get; set; } 
         public List<calificacion> Calificacion { get; set; } 
     }

b32ca204eae8d601bbb3530b78cee3a3744067aa
leonardopadillla <84750145+leonardopadillla@users.noreply.github.com>
2023-04-22 09:14:03 -0600

    Modelos
    Se crearon los modelos

---
+++ Models/publicaciones.cs
@@ -0,0 +1,9 @@
+namespace LP022018UP6012019MA603.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/roles.cs
@@ -0,0 +1,6 @@
+﻿namespace LP022018UP6012019MA603.Models
+{
+    public class comentario
+    {
+    }
+}
